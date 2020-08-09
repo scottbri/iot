888d1547a80e17774b4afcd9809f4a53ca192f2f
Scott Brightwell <scott@brightwell.org>
2020-08-09 22:29:48 +0000

    working

--- client/app.py
+++ client/app.py
@@ -2,9 +2,11 @@
 import csv, requests, os
 
 deviceId = 'device01'
-fileName = deviceId + '.csv'
+localFileName = 'local-data-log.csv'
+remoteFileName = deviceId + '.csv'
 upstreamNode = 'http://127.0.0.1:5000'
 sendUrl = upstreamNode + '/api/v1.0/receive'
+DEBUG = True
 
 #@app.route('/api/v1.0/receive', methods=['POST'])
 #        abort(400)
@@ -14,22 +16,29 @@ sendUrl = upstreamNode + '/api/v1.0/receive'
 def readSensor(deviceId):
     salinityData = '45%'
     tempData = '3.14159'
-    return [deviceId, salinityData, tempData]
+    logData = [deviceId, salinityData, tempData]
 
-def storeData(fileName, sensorData):
-    with open(fileName, 'a', newline='') as csvfile:
+    if DEBUG: print(logData) 
+    return logData
+
+def storeData(localFileName, sensorData):
+    with open(localFileName, 'a', newline='') as csvfile:
         #logWriter = csv.writer(csvfile, delimiter=',', quotechar='', quoting=csv.QUOTE_MINIMAL)
         logWriter = csv.writer(csvfile, delimiter=',')
         logWriter.writerow(sensorData)
+    
+    if DEBUG: print('Wrote ' + str(sensorData) + " to " + localFileName)
 
-def postData(fileName, sendUrl):
-    files = {'file': (fileName, open(fileName, 'rb'))}
+def postData(localFileName, remoteFileName, sendUrl):
+    files = {'file': (remoteFileName, open(localFileName, 'rb'))}
     r = requests.post(sendUrl, files=files)
+
+    if DEBUG: print('POST status code ' + str(r.status_code))
     if r.status_code == requests.codes.ok:
-        prevFile = 'prev' + fileName
+        prevFile = 'prev-' + localFileName
         os.remove(prevFile)
-        os.rename(fileName, prevFile)
+        os.rename(localFileName, prevFile)
 
 if __name__ == '__main__':
-    storeData(fileName, readSensor(deviceId))
-    postData(fileName, sendUrl)
+    storeData(localFileName, readSensor(deviceId))
+    postData(localFileName, remoteFileName, sendUrl)
--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 #!venv/bin/python
-from flask import Flask, request
+from flask import Flask, request, make_response
 import os
 
 app = Flask(__name__)
@@ -19,17 +19,19 @@ def receiveData():
         file_deviceID = os.path.splitext(fileName)[0]
         # filename porttion should be a deviceId
         if file_deviceID not in deviceIds:
-            abort(400)
+            print('Upload attempted by: ' + str(file_deviceID))
+            return(make_response('device not allowed', 404))
         else:
             # finally success.  deal with the incoming file
             # TODO perhaps crack open the file and add timestamps?
             # TODO change the save directory or filename before saving?
             uploaded_file.save(fileName)
             # TODO return the proper success status
-            return(200)
+            return('got it')
     else:
         # filename was empty string.  bail out
-        abort(400)
+        print('Upload filename not specified.  Ignoring.')
+        return(make_response('filename not specified', 500))
 
 if __name__ == '__main__':
     app.run(debug=True)
